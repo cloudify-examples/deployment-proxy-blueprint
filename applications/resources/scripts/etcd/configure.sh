71445a755b7acf44633e5a9eaa6c09396efc102c
earthmant <earthmant@icloud.com>
2017-04-27 14:40:56 +0300

    correcting something

--- applications/resources/scripts/etcd/configure.sh
+++ applications/resources/scripts/etcd/configure.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set +e
+
 DISCOVERY_URL=$(ctx instance runtime-properties DISCOVERY_URL)
 
 if [[ ! -z ${DISCOVERY_URL} ]]; then
+    exit 0;
+else
     CLUSTER_SIZE=1
     DISCOVERY_URL=$(curl https://discovery.etcd.io/new?size=${CLUSTER_SIZE})
     ctx instance runtime-properties DISCOVERY_URL $DISCOVERY_URL
-else
-    exit 0;
 fi
+
+set -e
--- applications/resources/scripts/etcd/delete.sh
+++ applications/resources/scripts/etcd/delete.sh
@@ -2,7 +2,5 @@
 
 # source: https://github.com/coreos/etcd/releases
 
-set -e
-
 sudo rm -r /bin/etcd
 sudo rm -r /bin/etcdctl
