f241a39b2cb4a5fa3a762997a6810045f0ce7e76
earthmant <earthmant@icloud.com>
2017-04-27 20:26:10 +0300

    enabling cluster size

--- imports/resources/scripts/etcd/configure.sh
+++ imports/resources/scripts/etcd/configure.sh
@@ -6,7 +6,6 @@ CLUSTER_SIZE=$(ctx node properties cluster_size)
 if [[ ! -z ${DISCOVERY_URL} ]]; then
     exit 0;
 else
-    CLUSTER_SIZE=1
     DISCOVERY_URL=$(curl https://discovery.etcd.io/new?size=${CLUSTER_SIZE})
     ctx instance runtime-properties DISCOVERY_URL $DISCOVERY_URL
 fi
